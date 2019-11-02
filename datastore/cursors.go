7ee1398356d778a4954052e24dba805e58f9fb49
Nico Huysamen <nico@huysamen.org>
2019-11-02 07:05:25 +0200

    Pointers for structs

--- datastore/cursors.go
+++ datastore/cursors.go
@@ -10,10 +10,14 @@ import (
 	"cloud.google.com/go/datastore"
 )
 
-func NewCursorCodecs() (encoder func(datastore.Cursor) (string, error), decoder func(string) (datastore.Cursor, error)) {
+func NewCursorCodecs() (encoder func(*datastore.Cursor) (string, error), decoder func(string) (*datastore.Cursor, error)) {
 	key := encryption.NewEncryptionKey(os.Getenv("GCPKIT_DATASTORE_CURSOR_SECRET"))
 
-	enc := func(cursor datastore.Cursor) (string, error) {
+	enc := func(cursor *datastore.Cursor) (string, error) {
+		if cursor == nil {
+			return "", errors.New("nil cursor")
+		}
+
 		encrypted, err := encryption.Encrypt([]byte(cursor.String()), key)
 		if err != nil {
 			return "", err
@@ -22,27 +26,27 @@ func NewCursorCodecs() (encoder func(datastore.Cursor) (string, error), decoder 
 		return base64.URLEncoding.EncodeToString(encrypted), nil
 	}
 
-	dec := func(encoded string) (datastore.Cursor, error) {
+	dec := func(encoded string) (*datastore.Cursor, error) {
 		if encoded == "" {
-			return datastore.Cursor{}, errors.New("cursor not valid")
+			return nil, errors.New("cursor not valid")
 		}
 
 		decoded, err := base64.URLEncoding.DecodeString(encoded)
 		if err != nil {
-			return datastore.Cursor{}, err
+			return nil, err
 		}
 
 		decrypted, err := encryption.Decrypt(decoded, key)
 		if err != nil {
-			return datastore.Cursor{}, err
+			return nil, err
 		}
 
 		cursor, err := datastore.DecodeCursor(string(decrypted))
 		if err != nil {
-			return datastore.Cursor{}, err
+			return nil, err
 		}
 
-		return cursor, nil
+		return &cursor, nil
 	}
 
 	return enc, dec
